e83af979ba423a08e17309240063242dadf4ad55
Serge Vakulenko <serge.vakulenko@gmail.com>
2024-07-11 23:38:58 -0700

    Move e50 and e57 extracodes to separate source files.

---
+++ e50.cpp
@@ -0,0 +1,262 @@
+//
+// Extracode 050 - elementary math functions and other services.
+//
+// Copyright (c) 2023-2024 Serge Vakulenko
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+
+#include "machine.h"
+
+//
+// Check whether value is large enough to be printed
+// in fixed format with given precision.
+//
+static bool good_for_fixed_format(double value, int precision)
+{
+    if (value < 0) {
+        value = -value;
+    }
+    if (value == 0) {
+        return true;
+    }
+    if (value >= 1) {
+        return true;
+    }
+    value *= std::pow(10, precision);
+    return (value >= 1);
+}
+
+//
+// Extracode e50 15: format real number.
+//
+Word Processor::e50_format_real(Word input, unsigned &overflow)
+{
+    E50_Format_Info info;
+    info.word = input;
+    machine.trace_e50_format_real(info);
+
+    const bool right_align   = info.field.right_align;
+    const unsigned width     = info.field.width;
+    const unsigned precision = info.field.precision;
+
+    // Source value.
+    const unsigned src_addr = ADDR(info.field.src_addr + core.M[info.field.src_reg]);
+    const double value      = besm6_to_ieee(machine.mem_load(src_addr));
+
+    // Destination pointer.
+    static unsigned dest_addr;
+    if (info.field.dest_addr != 0 || info.field.dest_reg != 0)
+        dest_addr = ADDR(info.field.dest_addr + core.M[info.field.dest_reg]);
+    BytePointer bp(memory, dest_addr);
+
+    if (width == 0) {
+        // Nothing to do.
+        overflow = 0;
+        return 0;
+    }
+
+    // Format as fixed point or as scientific.
+    std::ostringstream scientific;
+    scientific << std::scientific << std::uppercase << std::setprecision(precision) << value;
+    std::string result = scientific.str();
+
+    if (good_for_fixed_format(value, precision)) {
+        std::ostringstream fixed_point;
+        fixed_point << std::fixed << std::setprecision(precision) << value;
+
+        if (fixed_point.str().size() <= scientific.str().size()) {
+            // Fixed point format is shorter.
+            result = fixed_point.str();
+        }
+    }
+
+    overflow = (result.size() > width);
+    if (!right_align) {
+        // Align to the left.
+        if (result.size() < width) {
+            result.append(width - result.size(), ' ');
+        } else if (overflow) {
+            result.erase(width);
+        }
+    } else if (overflow) {
+        // Align to the right, skip first few bytes.
+        result.erase(0, result.size() - width);
+    } else if (result.size() < width) {
+        // Align to the right, fill with spaces.
+        result.insert(0, width - result.size(), ' ');
+    }
+
+    for (char ch : result) {
+        bp.put_byte(ch);
+    }
+
+    // Fill last word with zeroes.
+    while (bp.byte_index) {
+        bp.put_byte(' ');
+    }
+    return width;
+}
+
+//
+// Extracode 050: elementary math functions and other services.
+//
+void Processor::e50()
+{
+    auto addr = core.M[016];
+    switch (addr) {
+    case 0:
+        core.ACC = besm6_sqrt(core.ACC);
+        break;
+    case 1:
+        core.ACC = besm6_sin(core.ACC);
+        break;
+    case 2:
+        core.ACC = besm6_cos(core.ACC);
+        break;
+    case 3:
+        core.ACC = besm6_arctan(core.ACC);
+        break;
+    case 4:
+        core.ACC = besm6_arcsin(core.ACC);
+        break;
+    case 5:
+        core.ACC = besm6_log(core.ACC);
+        break;
+    case 6:
+        core.ACC = besm6_exp(core.ACC);
+        break;
+    case 7:
+        core.ACC = besm6_floor(core.ACC);
+        break;
+    case 017:
+        // Format real numbers.
+        core.ACC = e50_format_real(core.ACC, core.M[14]);
+        break;
+    case 064:
+        // Print some message.
+        // TODO: print_iso(ADDR(core.ACC));
+        break;
+    case 066:
+        // Get reply from operator.
+        // Change page on plotter.
+        machine.plotter.change_page();
+        core.ACC = 0;
+        break;
+    case 067: {
+        // DATE*, OS Dubna specific.
+        // Always return the same date/time, for easy testing.
+        static const Word     DAY   = 0x04;
+        static const Word     MONTH = 0x07; // July
+        static const Word     YEAR  = 0x24;
+        static const unsigned HOUR  = 0x23;
+        static const unsigned MIN   = 0x45;
+        static const unsigned SEC   = 0x56;
+
+        // Date: DD MON YY
+        //        |  |   |
+        //       42  34  26 - shift
+        // Time: 00.00.00
+        //        |  |  |
+        //       20  16 4 - shift
+        core.ACC = (DAY << 42) | (MONTH << 34) | (YEAR << 26) |
+                   (HOUR << 20) | (MIN << 12) | (SEC << 4);
+        break;
+    }
+    case 075:
+        // Unknown.
+        break;
+    case 076:
+        // Send message to operator.
+        // TODO: print_iso(ADDR(core.ACC));
+        break;
+    case 0102:
+        // Some conversion?
+        break;
+    case 0103:
+        // TODO: Intercept авост, for Forex.
+        break;
+    case 0202:
+        // Convert tape number from internal format into 2-10 format.
+        break;
+    case 0203:
+        // Convert tape number from 2-10 format into internal format.
+        break;
+    case 0210:
+        // TODO: Lock/release semaphores.
+        break;
+    case 0211:
+        // Pause the task? Waiting for tape.
+        throw Exception("Task paused waiting for tape");
+        break;
+    case 070077:
+        // Get date?
+        core.ACC = 0;
+        break;
+    case 070200:
+        // Asking for some capabilities?
+        core.ACC = 0'0010'0000;
+        break;
+    case 070210:
+        // Get time?
+        core.ACC = 0;
+        break;
+    case 070214:
+        // Asking for шифр?
+        core.ACC = 0'1234'5670'1234'5670;
+        break;
+    case 070217:
+        // Unknown
+        break;
+    case 071223:
+        // Unknown, for Forex.
+        break;
+    case 072200:
+        // Unknown, for Dipol.
+        break;
+    case 072211:
+        // Set time limit?
+        // TODO: show time limit on core.ACC
+        break;
+    case 072214:
+        // Set something for шифр?
+        break;
+    case 072216:
+        // Set paper limit?
+        // TODO: show paper limit on core.ACC
+        break;
+    case 074200:
+        // Unknown, for *page
+        break;
+    case 074671:
+        // Unknown
+        break;
+    case 074673:
+        // Unknown
+        break;
+    case 076200:
+        // Unknown, for *tape
+        break;
+    default:
+        throw Exception("Unimplemented extracode *50 " + to_octal(addr));
+    }
+}
---
+++ e57.cpp
@@ -0,0 +1,213 @@
+//
+// Extracode 057 - floor() function, calcomp plotter, file i/o.
+//
+// Copyright (c) 2023-2024 Serge Vakulenko
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+#include "machine.h"
+
+//
+// Extracode 057.
+//
+void Processor::e57()
+{
+    auto addr = core.M[016];
+    switch (addr) {
+    case 0:
+        core.ACC = besm6_floor(core.ACC);
+        return;
+    case 2:
+        // Output to Calcomp plotter.
+        machine.plotter.calcomp_putch(core.ACC);
+        core.ACC = 0;
+        return;
+    case 3:
+    case 7:
+        // Delay the task, presumably waiting for tape to be installed by operator.
+        throw Exception("Task paused waiting for tape");
+    case 5:
+        // Unknown, for Forex.
+        core.ACC = 0;
+        return;
+    case 077777:
+        // Manage discs and files.
+        e57_file();
+        return;
+    default:
+        if (addr >= 010) {
+            // Manage tapes.
+            e57_tape();
+            return;
+        }
+        throw Exception("Unimplemented extracode *57 " + to_octal(addr));
+    }
+}
+
+//
+// Extracode 057 10...7777: mount tapes.
+//
+void Processor::e57_tape()
+{
+    // Modes of *57 in address field.
+    enum {
+        NOTFOUND = 010,   // печать 'нет магнитной ленты'
+        BUSY     = 020,   // печать 'занят магнитофон'
+        READY    = 040,   // печать 'нe гoтов магнитофон'
+        WRITE    = 0100,  // печать 'запись'
+        READ     = 0200,  // печать 'чтение'
+        NODIAG   = 0400,  // блокировка любой печати
+        BYNAME   = 01000, // поиск только по имени (без N бобины)
+        ASSIGN   = 02000, // поиск c захватом ленты (мат.номер ленты - в 13 регистре)
+        RELEASE  = 04000, // отказ от cвоиx лент, заданныx на сумматоре битовой шкалой:
+    };                    // 48 разряд - лента 30 для мат.задач
+    auto addr = core.M[016];
+
+    if (addr & ASSIGN) {
+        //
+        // Mount tape (by name and number) on given disk number.
+        // Disk number is provided in register #13.
+        //
+        bool write_permit = addr & WRITE;
+        machine.disk_mount(core.M[015], core.ACC, write_permit);
+        core.ACC = core.M[015];
+        return;
+    }
+
+    if (addr & RELEASE) {
+        //
+        // Release tapes according to bitmask on accumulator.
+        //
+        if (addr & READY) {
+            // Flush output buffers?
+        } else {
+            machine.disk_release(core.ACC);
+        }
+        core.ACC = 0;
+        return;
+    }
+
+    //
+    // Find mounted tape (by name and number).
+    // Return disk number in range 030-077.
+    //
+    core.ACC = machine.disk_find(core.ACC);
+}
+
+//
+// Extracode 057 77777: mount files.
+//
+void Processor::e57_file()
+{
+    // Use control word at accumulator.
+    E57_File_Info info;
+    info.word = core.ACC;
+
+    machine.trace_e57_file(info);
+
+    if ((info.word & E57_File_Info::KEY_BITMASK) != E57_File_Info::KEY_VALUE) {
+        throw Exception("Wrong access key in *57 77777");
+    }
+    switch (info.field.op) {
+    case E57_File_Info::VOLUME_OPEN:
+        // Disk name is located at addr+1.
+        // Accept any disk name for now.
+        core.ACC = 0;
+//
+// заказ обычных файлов
+//
+// формируем стек вот с такой структурой:
+//     1. признак монополии на мд               0000 0000 0000 0000
+//     2. имя пакета                            4657 5700 0000 0001
+//     3. запрос координат одного файла
+//     4. запрос координат след.файла
+//        .........................
+//        запрос координат последнего файла
+//        бит 48                                4000 0000 0000 0000
+//
+// запрос координат файла выглядит так:
+//     1. имя хозяина файла                     0000 0000 0000 0000
+//     2. имя файла                             2044 0522 1002 0040
+//     3. поле ответа (+бит 30, если запись)    0000 0040 0000 0000
+//     4. бит 48 + мат.номер файла в 1:6 р.     4000 0000 0000 0041
+//
+        break;
+    case E57_File_Info::VOLUME_RELEASE:
+        throw Exception("Extracode *57 77777: operation 'Release Volume' not supported yet");
+        break;
+    case E57_File_Info::FILE_SEARCH:
+        // File name is located at addr.
+        //throw Exception("Extracode *57 77777: operation 'Search File' not supported yet");
+        // Accept for now.
+        core.ACC = 0;
+//
+// в поле ответа для каждого файла
+// будут его координаты на дп
+// а также признаки всяких бяк:
+//     вiт48 - плохое обращение
+//     вiт47 - файла нет на дп
+//     вiт46 - нет доступа к файлу
+//
+        break;
+    case E57_File_Info::FILE_OPEN:
+        // File name is located at addr.
+        //throw Exception("Extracode *57 77777: operation 'Open File' not supported yet");
+        // Accept for now.
+        core.ACC = 0;
+//
+// формируется стек для похода на заказ файлов (т=3)
+//     1. имя дп
+//     2. координаты первого файла (берутся из поля ответа предыдущего стека + мат.номер в 37:42 р.)
+//        ...........................
+//        координаты последнего файла
+//        нулевая ячейка
+//
+// в ячейках по файлам:
+//     бит 43 - нет пакета
+//     бит 44 - занят мат.номер
+//     бит 45 - занят файл
+//     бит 46 - нет места в fата
+//
+        break;
+    case E57_File_Info::SCRATCH_OPEN:
+        throw Exception("Extracode *57 77777: operation 'Open Scratch' not supported yet");
+//
+// структура инф.поля такова:
+//     1. хар-ка sсr-файла
+//          1:5 - кол-во экстентов
+//          37:42 - мат.номер
+//     2. хар-ка следующего файла
+//        ........................
+//        нулевая ячейка
+//
+        break;
+    case E57_File_Info::FILE_RELEASE:
+        throw Exception("Extracode *57 77777: operation 'Release File' not supported yet");
+        break;
+    case E57_File_Info::ALL_RELEASE:
+        throw Exception("Extracode *57 77777: operation 'Release All' not supported yet");
+        break;
+    case E57_File_Info::FILE_CONTROL:
+        // File name is located at addr.
+        throw Exception("Extracode *57 77777: operation 'Change File Status' not supported yet");
+        break;
+    default:
+        throw Exception("Extracode *57 77777: unknown operation " + to_octal(info.field.op));
+    }
+}
